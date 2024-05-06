f6b0c9e0d3af4b044a23e5ad51db7f0c52cc0c2a
Natalia Ladino <naladino@personalsoft.com.co>
2024-05-06 09:10:09 -0500

    base

---
+++ Enums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitHubNet
+{
+    public enum Enums
+    { 
+        /// <summary>
+        /// Value to type primary
+        /// </summary>
+        Primary = 1,
+
+        /// <summary>
+        /// Value to type secondary
+        /// </summary>
+        Secondary = 2,
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TestGitHubNet
+{
+    public class Utils
+    {
+        public static int CountRegexMatchs(string text, string regex)
+        {
+            int count = Regex.Count(text, regex);
+
+            return count;
+        }
+    }
+}
